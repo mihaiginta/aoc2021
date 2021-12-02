1cfb2baafee11670bac240d6238270a534ffd3d4
Mihai Ginta <sabin-mihai.ginta@dlr.de>
2021-12-02 10:53:52 +0100

    Solve task 2

--- day2/run.py
+++ day2/run.py
@@ -4,7 +4,7 @@ def task1(filename):
         vertivalValues = 0
         forwardValues = 0
         for line in f:
-            print(line.rstrip())
+            #print(line.rstrip())
             if 'up' in line:
                 vertivalValues -= int(line.split()[1])
             elif 'down' in line:
@@ -15,4 +15,27 @@ def task1(filename):
     return position
 
 position = task1("input.txt")
+print(position)
+
+
+
+# %%
+def task2(filename):
+    with open(filename, "r") as f:
+        depth = 0
+        horizontalPosition = 0
+        aim = 0
+        for line in f:
+            #print(line.rstrip())
+            if 'up' in line:
+                aim -= int(line.split()[1])
+            elif 'down' in line:
+                aim += int(line.split()[1])
+            elif 'forward' in line:
+                horizontalPosition += int(line.split()[1])
+                depth += aim*int(line.split()[1])
+    position = depth*horizontalPosition 
+    return position
+
+position = task2("input.txt")
 print(position)
--- day2/test_run.py
+++ day2/test_run.py
@@ -1,9 +1,13 @@
 import pytest
 
-from run import task1
+from run import task1, task2
 
 #from run import task1
 
 def test_task1():
     position = task1("example1.txt")
     assert position==150
+
+def test_task2():
+    position = task2("example1.txt")
+    assert position==900
