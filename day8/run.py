ba50533ecf5ab63d14d793bca02d8d2485cd4404
Mihai Ginta <sabin-mihai.ginta@dlr.de>
2021-12-09 16:25:00 +0100

    Solve task 8.2 with scikit-image

--- day8/run.py
+++ day8/run.py
@@ -1,6 +1,7 @@
 # %%
 import numpy as np
 import os
+import collections
 os.chdir(os.path.dirname(__file__))
 with open("input.txt", "r") as f:
     matrix = np.array([list(line.strip()) for line in f.readlines()], dtype=int)
@@ -12,7 +13,18 @@ for iRow in range(1, nRows-1):
         if (paddedMatrix[iRow][iColumn] < paddedMatrix[iRow-1][iColumn] and
             paddedMatrix[iRow][iColumn] < paddedMatrix[iRow+1][iColumn] and
             paddedMatrix[iRow][iColumn] < paddedMatrix[iRow][iColumn+1] and
-                paddedMatrix[iRow][iColumn] < paddedMatrix[iRow][iColumn-1]):
+            paddedMatrix[iRow][iColumn] < paddedMatrix[iRow][iColumn-1]):
             sumRiskLevel += paddedMatrix[iRow][iColumn]+1
 print(sumRiskLevel)
+# %%
+paddedMatrix[paddedMatrix!=9]=True
+paddedMatrix[paddedMatrix==9]=False
+#%%
+from skimage import measure
+all_labels = measure.label(paddedMatrix, connectivity=1)
+print(all_labels)
+#%%
+unique, counts = np.unique(all_labels, return_counts=True)
+first3Counts = sorted(counts, reverse=True)[1:4]
+print(np.prod(first3Counts))
 # %%
